76895c31413a6148b4d8b7f5dd4e9ef1963b40ec
afnan raafat <121581251+afnan-raafat@users.noreply.github.com>
2025-01-30 01:56:46 +0200

    fetch data with useMutation ^-^

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,30 +1,50 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { useQuery } from 'react-query'
+import { useMutation, useQuery } from 'react-query'
 import './App.css'
 
 export default function Todo() {
-    const { data, isLoading } = useQuery({
-      queryKey: ["todo"],
-      queryFn: () => fetch("https://jsonplaceholder.typicode.com/todos").then((res)=>res.json()),
-    });
-   // console.log(data);
+    //  with useMutation () it take fun only
+    const getMutation = useMutation({
+        mutationFn: () => fetch("https://jsonplaceholder.typicode.com/todos").then((res) => res.json()),
+    }
+    );
+ 
+  //  with useQuery () it take key && fun
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ["todo"],
+  //   queryFn: () => fetch("https://jsonplaceholder.typicode.com/todos").then((res)=>res.json()),
+  // });
+  //    console.log(data);
 
     return (
-      
-        <div className='container'>
-
-            {data &&
-                data?.map((todo) => (
-                    <div className={`todo ${todo.completed ? "Done" : "Pendding"}`} key={todo.id}>
-                        
+      <>
+        <button onClick={() => getMutation.mutate()}> Get Todos </button>
+        <div className="container">
+          {getMutation.data &&
+            getMutation.data?.map((todo) => (
+              <div
+                className={`todo ${todo.completed ? "Done" : "Pendding"}`}
+                key={todo.id}
+              >
                 {todo.title}
-                        <p>Status : {todo.completed ?"Done":"Pendding" }</p>
-
-            </div>
+                <p>Status : {todo.completed ? "Done" : "Pendding"}</p>
+              </div>
             ))}
-            
-
+      {/* When use useQuery */}
+          {/*
+        {data &&
+  data?.map((todo) => (
+    <div
+      className={`todo ${todo.completed ? "Done" : "Pendding"}`}
+      key={todo.id}
+    >
+      {todo.title}
+      <p>Status : {todo.completed ? "Done" : "Pendding"}</p>
+    </div>
+  ))}
+       */}
         </div>
-  )
+      </>
+    );
 }
